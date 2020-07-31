3882c6c326af1af83aa5d9595bfaa30be02bfc61
玄冰 <wb-yqz694677@alibaba-inc.com>
2020-07-31 15:56:16 +0800

    口令红包

--- sol/CommandWelfareContract.sol
+++ sol/CommandWelfareContract.sol
@@ -6,22 +6,19 @@ interface Token {
 }
 
 contract CommandWelfareContract {
-    string command;
     mapping(string => bool) commandMap;
-
+    string command;
     uint8 totalNum;
     uint8 remnantNum;
-
     address owner;
-    // 0 已创建，1可领取，2已撤回
     uint8 status;
-
     Token token;
-
-    // 领奖记录
     mapping(address => uint) robWelfareMap;
     address[] accountList;
 
+     // _command 口令
+     // num 数量
+     // _token 20代币地址 0x87010faf5964d67ed070bc4b8dcafa1e1adc0997
     constructor(string memory _command, uint8 num, Token _token) public {
         owner = msg.sender;
         command = _command;
@@ -31,6 +28,7 @@ contract CommandWelfareContract {
         token = _token;
     }
 
+    // 发送红包
     function send() public {
         require(msg.sender == owner, "only owner");
         require(status == 0, "status is not 0");
@@ -42,6 +40,7 @@ contract CommandWelfareContract {
         status = 1;
     }
 
+    // 撤销红包
     function withdraw() public {
         require(msg.sender == owner, "only owner");
 
@@ -49,6 +48,7 @@ contract CommandWelfareContract {
         token.transfer(owner, token.balanceOf(address(this)));
     }
 
+    // 领取红包
     function robWelfare(string memory _command) public {
         require(commandMap[_command], "command is fail");
         require(status == 1, "status is not 1");
@@ -65,12 +65,13 @@ contract CommandWelfareContract {
         remnantNum --;
     }
 
+    // 红包信息
+    // _status 0 已创建，1可领取，2已撤回
     function getWelfareInfo(address account) public view returns(
-        string memory _command,
-        uint8 _totalNum,
-        uint8 _remnantNum,
-        uint256 _amount,
-        address _owner,
+        string memory _command, // 口令
+        uint8 _totalNum, // 总数
+        uint8 _remnantNum, // 剩余数量
+        uint256 _amount, // 剩余金额
         uint8 _status) {
 
         string memory tempCommand;
@@ -81,9 +82,10 @@ contract CommandWelfareContract {
         }
 
         uint amount = token.balanceOf(address(this));
-        return (tempCommand, totalNum, remnantNum, amount, owner, status);
+        return (tempCommand, totalNum, remnantNum, amount, status);
     }
 
+    // 获取已领取名单
     function getAccountList() public view returns(address[] memory) {
         return accountList;
     }
